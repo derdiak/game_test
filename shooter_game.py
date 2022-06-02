517b95fc552d9adca5c89e976f05bc297a1e7a0e
derdiak <56913753+derdiak@users.noreply.github.com>
2022-06-02 18:41:17 +0300

    Add files via upload

---
+++ shooter_game.py
@@ -0,0 +1,55 @@
+from pygame import *
+from random import randint
+#подгружаем отдельно функции для работы со шрифтом
+font.init()
+font1 = font.SysFont('Arial', 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
+font2 = font.SysFont('Arial', 36)
+
+#фоновая музыка
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('fire.ogg')
+
+#нам нужны такие картинки:
+img_back = "galaxy.jpg" #фон игры
+img_bullet = "bullet.png" #пуля
+img_hero = "rocket.png" #герой
+img_enemy = "ufo.png" #враг
+score = 0 #сбито кораблей
+goal = 10 #столько кораблей нужно сбить для победы
+lost = 0 #пропущено кораблей
+max_lost = 3 #проиграли, если пропустили столько
+#класс-родитель для других спрайтов
+
+#создаём окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+finish = False
+#основной цикл игры:
+run = True #флаг сбрасывается кнопкой закрытия окна
+while run:
+    #событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    #сама игра: действия спрайтов, проверка правил игры, перерисовка
+    if not finish:
+        #обновляем фон
+        window.blit(background,(0,0))
+
+        #пишем текст на экране
+        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
+    display.update()
+    time.delay(50)
